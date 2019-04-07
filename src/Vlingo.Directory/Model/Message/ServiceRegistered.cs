7a05ceb00ae2a9833f379b496bf5297d54adcdb8
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-04-07 21:31:05 +0200

    Implement ServiceRegistered and unit tests

---
+++ src/Vlingo.Directory/Model/Message/ServiceRegistered.cs
@@ -0,0 +1,67 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
+using System.Text;
+using Vlingo.Wire.Message;
+using Vlingo.Wire.Node;
+
+namespace Vlingo.Directory.Model.Message
+{
+    public class ServiceRegistered : IMessage
+    {
+        private readonly HashSet<Address> _addresses;
+            
+        public static string TypeName { get; } = "SRVCREGD";
+
+        public IEnumerable<Address> Addresses => _addresses;
+
+        public bool IsValid => !Name.HasNoName;
+        
+        public Name Name { get; }
+
+        public static ServiceRegistered From(string content)
+        {
+            if (content.StartsWith(TypeName))
+            {
+                var name = MessagePartsBuilder.NameFrom(content);
+                var addresses = MessagePartsBuilder.AddressFromRecord(content, AddressType.Main);
+                return new ServiceRegistered(name, addresses);
+            }
+            
+            return new ServiceRegistered(Name.NoNodeName);
+        }
+
+        public static ServiceRegistered As(Name name, Address address) => new ServiceRegistered(name, address);
+        
+        public static ServiceRegistered As(Name name, IEnumerable<Address> addresses) => new ServiceRegistered(name, addresses);
+
+        public ServiceRegistered(Name name, Address address) : this(name) => _addresses.Add(address);
+
+        public ServiceRegistered(Name name, IEnumerable<Address> addresses) : this(name) => _addresses.UnionWith(addresses);
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            
+            builder.Append(TypeName).Append("\n").Append("nm=").Append(Name.Value);
+
+            foreach (var address in _addresses)
+            {
+                builder.Append("\n").Append(AddressType.Main.Field).Append(address.Host.Name).Append(":").Append(address.Port);
+            }
+
+            return builder.ToString();
+        }
+
+        private ServiceRegistered(Name name)
+        {
+            Name = name;
+            _addresses = new HashSet<Address>();
+        }
+    }
+}
