3b8850fc8b17607297b7028ae92d453860099d70
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-11-13 21:49:45 +0100

    Add retires

--- src/Vlingo.Directory.Tests/Model/DirectoryServiceTest.cs
+++ src/Vlingo.Directory.Tests/Model/DirectoryServiceTest.cs
@@ -260,13 +260,13 @@ namespace Vlingo.Directory.Tests.Model
             
             Pause();
 
-            Assert.Equal(3, accessSafely1.ReadFromExpecting("interestedIn", 3));
-            Assert.Equal(3, accessSafely2.ReadFromExpecting("interestedIn", 3));
-            Assert.Equal(3, accessSafely3.ReadFromExpecting("interestedIn", 3));
+            Assert.Equal(3, accessSafely1.ReadFromExpecting("interestedIn", 3, 10));
+            Assert.Equal(3, accessSafely2.ReadFromExpecting("interestedIn", 3, 10));
+            Assert.Equal(3, accessSafely3.ReadFromExpecting("interestedIn", 3, 10));
             
-            Assert.Equal(3, accessSafely1.ReadFromExpecting("informDiscovered", 3));
-            Assert.Equal(3, accessSafely2.ReadFromExpecting("informDiscovered", 3));
-            Assert.Equal(3, accessSafely3.ReadFromExpecting("informDiscovered", 3));
+            Assert.Equal(3, accessSafely1.ReadFromExpecting("informDiscovered", 3, 10));
+            Assert.Equal(3, accessSafely2.ReadFromExpecting("informDiscovered", 3, 10));
+            Assert.Equal(3, accessSafely3.ReadFromExpecting("informDiscovered", 3, 10));
 
             foreach (var interest in _interests)
             {
