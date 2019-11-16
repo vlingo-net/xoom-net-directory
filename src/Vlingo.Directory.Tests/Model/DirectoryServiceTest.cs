26a31f4714306dfcf588c48587ba20df8a89ff58
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-11-16 11:23:23 +0100

    Tweaking for CI to check why tests fails

--- src/Vlingo.Directory.Tests/Model/DirectoryServiceTest.cs
+++ src/Vlingo.Directory.Tests/Model/DirectoryServiceTest.cs
@@ -357,7 +357,7 @@ namespace Vlingo.Directory.Tests.Model
             _testWorld.Terminate();
         }
 
-        private void Pause(int milliseconds = 20000)
+        private void Pause(int milliseconds = 10000)
         {
             Thread.Sleep(milliseconds);
         }
--- src/Vlingo.Directory/Model/DirectoryServiceActor.cs
+++ src/Vlingo.Directory/Model/DirectoryServiceActor.cs
@@ -239,7 +239,7 @@ namespace Vlingo.Directory.Model
                 _cancellableMessageProcessing = Stage.Scheduler.Schedule(
                     SelfAs<IScheduled<IntervalType>>(),
                     IntervalType.Processing,
-                    TimeSpan.Zero,
+                    TimeSpan.FromMilliseconds(50), 
                     TimeSpan.FromMilliseconds(_timing.ProcessingInterval));
             }
 
@@ -248,7 +248,7 @@ namespace Vlingo.Directory.Model
                 _cancellablePublishing = Stage.Scheduler.Schedule(
                     SelfAs<IScheduled<IntervalType>>(),
                     IntervalType.Publishing,
-                    TimeSpan.Zero,
+                    TimeSpan.FromMilliseconds(50),
                     TimeSpan.FromMilliseconds(_timing.ProcessingInterval));
             }
         }
