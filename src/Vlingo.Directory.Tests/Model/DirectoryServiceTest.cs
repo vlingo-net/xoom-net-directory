2965aeed5b83e19e66685935f153933deda28954
Tomasz Jaskula <thomasv1000@hotmail.fr>
2019-11-17 23:47:54 +0100

    Enable AccessSafely for TestAlteredLeadership

--- src/Vlingo.Directory.Tests/Model/DirectoryServiceTest.cs
+++ src/Vlingo.Directory.Tests/Model/DirectoryServiceTest.cs
@@ -166,13 +166,13 @@ namespace Vlingo.Directory.Tests.Model
             var info3 = new ServiceRegistrationInfo("test-service3", new List<Location> { location3 });
             _client3.Actor.Register(info3);
 
-            Assert.Equal(3, accessSafely1.ReadFromExpecting("interestedIn", 3));
-            Assert.Equal(3, accessSafely2.ReadFromExpecting("interestedIn", 3));
-            Assert.Equal(3, accessSafely3.ReadFromExpecting("interestedIn", 3));
+            Assert.Equal(3, accessSafely1.ReadFromExpecting("interestedIn", 1));
+            Assert.Equal(3, accessSafely2.ReadFromExpecting("interestedIn", 1));
+            Assert.Equal(3, accessSafely3.ReadFromExpecting("interestedIn", 1));
             
-            Assert.Equal(3, accessSafely1.ReadFromExpecting("informDiscovered", 3));
-            Assert.Equal(3, accessSafely2.ReadFromExpecting("informDiscovered", 3));
-            Assert.Equal(3, accessSafely3.ReadFromExpecting("informDiscovered", 3));
+            Assert.Equal(3, accessSafely1.ReadFromExpecting("informDiscovered", 1));
+            Assert.Equal(3, accessSafely2.ReadFromExpecting("informDiscovered", 1));
+            Assert.Equal(3, accessSafely3.ReadFromExpecting("informDiscovered", 1));
             
             Pause();
 
