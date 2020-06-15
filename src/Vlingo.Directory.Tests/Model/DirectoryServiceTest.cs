372a11f376de2efa8f7de9e346b40ad0fb2b7786
Tomasz Jaskula <thomasv1000@hotmail.fr>
2020-06-15 12:23:10 +0200

    Set after completing to 3 for UT

--- src/Vlingo.Directory.Tests/Model/DirectoryServiceTest.cs
+++ src/Vlingo.Directory.Tests/Model/DirectoryServiceTest.cs
@@ -137,9 +137,9 @@ namespace Vlingo.Directory.Tests.Model
             // START directory assigned leadership
             _directory.Actor.AssignLeadership();
 
-            var accessSafely1 = _interest1.AfterCompleting(6);
-            var accessSafely2 = _interest2.AfterCompleting(6);
-            var accessSafely3 = _interest3.AfterCompleting(6);
+            var accessSafely1 = _interest1.AfterCompleting(3);
+            var accessSafely2 = _interest2.AfterCompleting(3);
+            var accessSafely3 = _interest3.AfterCompleting(3);
             
             var location1 = new Location("test-host1", PortToUse.GetAndIncrement());
             var info1 = new ServiceRegistrationInfo("test-service1", new List<Location> { location1 });
